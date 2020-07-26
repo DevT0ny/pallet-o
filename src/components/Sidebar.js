ba93afc3703ba689a32d3ceae265dd3ed5a370bd
Ajith-stark <ajith.kum.12342@gmail.com>
2020-07-26 19:36:55 +0530

    Slidebar link generation corrected

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,39 +3,35 @@ import { NavLink } from "react-router-dom";
 
 export default class SlideBar extends Component {
     state={
-        URLs:{
-            Clipboard:"/",
-            Pallets:"/pallets",
-            Shit:"/shit",
-            About : "/about"
-        }
+        urls:[
+          {title:"Clipboard" , url:"/"},
+          {title:"Pallets" , url:"/pallets"},
+          {title:"Shit" , url:"/shit"},
+          {title:"About" , url:"/about"}
+        ]
     }
-    links=()=>{
-      let nav =[]
-      for(let URL in this.state.URLs){
-        nav.push((
-          <li className="w-full " key={nav.length} >
-            <NavLink
-              exact to={this.state.URLs[URL]}
-              activeClassName="slidebar__link--active "
-              className="slidebar__link"
-            >
-            {URL}
-            </NavLink>
-          </li>
-        ))
-      }
-      return nav
-    }
+
     render() {
 
         return (
-            <nav className="w-48 slidebar h-screen fixed shadow-md">
-                <h2 className="slidebar__logo">Insta-color</h2>
-                <ul className="h-full w-full p-0 ">
-                    {this.links()}
-                </ul>
-            </nav>
+          <nav className="w-48 slidebar h-screen fixed shadow-md">
+              <h2 className="slidebar__logo">Insta-color</h2>
+              <ul className="h-full w-full p-0 ">
+                  {
+                    this.state.urls.map(({title,url},index)=>(
+                      <li className="w-full " key={index} >
+                        <NavLink
+                          exact to={url}
+                          activeClassName="slidebar__link--active "
+                          className="slidebar__link"
+                        >
+                        {title}
+                        </NavLink>
+                      </li>
+                    ))
+                  }
+              </ul>
+          </nav>
         )
     }
 }
