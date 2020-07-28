3ac14fa62bec0b2ea9ba65528d63fc3f0b46e7cf
Ajith-stark <ajith.kum.12342@gmail.com>
2020-07-28 22:54:54 +0530

    changed header component position form DOM since its action is diff for diff pages. removed contexts, pallets aka shade generator is working

--- src/components/Pallets.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-export default class Pallets extends Component {
-    render() {
-        return (
-            <div> pa;led
-            </div>
-        )
-    }
-}
---
+++ src/components/Shades.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import tinycolor from 'tinycolor2'
+import Header from './Header'
+
+
+export default function Shades() {
+
+  const [color, setColor] = useState('#ff0000')
+
+  var brightenList = [], darkenList=[], step =4;
+  for (let i=100; i>=0 ;i-=step) brightenList.push( tinycolor(color).brighten(i).toString() )
+  for (let i=0; i<=100 ;i+=step) darkenList.push(tinycolor(color).darken(i).toString())
+
+  var shadesList= [...brightenList,...darkenList ] ;
+  shadesList = [...new Set(shadesList)]
+
+  return (
+    <>
+      <Header addColor={setColor}/>
+
+        <div className="px-4 w-full --bg-indigo-700 gap-0 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" >
+
+          {
+            shadesList.map((color,inx_)=>{
+              return (
+                <div key={inx_}  className="w-full h-24  overflow-hidden relative " style={{backgroundColor:color}}>
+                  <div className="m-auto" style={{color:tinycolor(color).greyscale().toHexString()}} >{color}</div>
+                  <div className="m-auto" style={{color:tinycolor(color).lighten().desaturate().toHexString()}} >{color}</div>
+
+                </div>
+              )
+            })
+          }
+
+
+
+      </div>
+    </ >
+  )
+}
