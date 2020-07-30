9c9ecd7972819ea41763636a370eee54c275023b
Ajith-stark <ajith.kum.12342@gmail.com>
2020-07-30 16:12:05 +0530

    Updates

---
+++ precache-manifest.3a577e655932cdc6bb56c57c4b706a3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d117949c42c8cf6311a7de05fc106115",
+    "url": "/pallet-o/index.html"
+  },
+  {
+    "revision": "c16e16ad2c1c274495b4",
+    "url": "/pallet-o/static/css/main.908af701.chunk.css"
+  },
+  {
+    "revision": "4be696c512dae338cdd4",
+    "url": "/pallet-o/static/js/2.3d93c67a.chunk.js"
+  },
+  {
+    "revision": "6993fa7b13fb81f4bc70d0dc713d6897",
+    "url": "/pallet-o/static/js/2.3d93c67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16e16ad2c1c274495b4",
+    "url": "/pallet-o/static/js/main.cdedb137.chunk.js"
+  },
+  {
+    "revision": "5300274a73fee849d75a",
+    "url": "/pallet-o/static/js/runtime-main.701e7a04.js"
+  },
+  {
+    "revision": "efe1082e389992f585b34b471b6649cb",
+    "url": "/pallet-o/static/media/copy.efe1082e.svg"
+  },
+  {
+    "revision": "03110b31420d01fc5015248e125be56c",
+    "url": "/pallet-o/static/media/plus.03110b31.svg"
+  },
+  {
+    "revision": "ed3e6f19b43bd7069fa1317afdea5682",
+    "url": "/pallet-o/static/media/trash.ed3e6f19.svg"
+  }
+]);
